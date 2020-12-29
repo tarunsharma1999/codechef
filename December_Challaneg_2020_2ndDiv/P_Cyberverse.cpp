03f6a2d2d876c1879f5bca0ac872da0ad61eb05e
unknown <ts5153033@gmail.com>
2020-12-29 23:49:10 +0530

    new challange done

---
+++ December_Challaneg_2020_2ndDiv/P_Cyberverse.cpp
@@ -0,0 +1,18 @@
+//Cyberverse
+//https://www.codechef.com/problems/CYBV
+#include<string.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,k;
+		cin>>n>>k;
+		cout<<<<k/n<<endl;
+	}
+	return 0;
+}
